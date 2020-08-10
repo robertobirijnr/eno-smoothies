f7be825b8626071ef0c18ae4aa3022b5c76c5d64
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-10 22:00:45 +0000

    navbar and body

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons trash" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <h2 class="indigo-text">{{smoothie.title}}</h2>
+        <div class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ing}}</span>
+          </li>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      smoothies: [
+        {
+          title: "Ninja Brew",
+          slug: "nonja-brew",
+          ingredients: ["banana", "coffe", "milk", "yoghurt"],
+          id: "1",
+        },
+        {
+          title: "Morning Mood",
+          slug: "morning-mood",
+          ingredients: ["mamgo", "lime", "juice"],
+          id: "2",
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter((smoothie) => {
+        return smoothie.id != id;
+      });
+    },
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+
+h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+
+.ingredients {
+  margin: 30px auto;
+}
+
+.ingredients li {
+  display: inline-block;
+}
+
+.trash {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
+</style>
+
