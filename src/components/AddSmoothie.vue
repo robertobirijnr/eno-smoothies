3c6d8db72f5d976648a89542fa6a4a7bb5665733
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-11 13:20:36 +0000

    create smoothies

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="addSmoothie container">
     <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <p class="pink-text">
+      <i>nb: use the tab key to add more ingredients to your smoothie</i>
+    </p>
     <form @submit.prevent="addSmoothie">
       <div class="field title">
         <label for="title">Smoothie Title</label>
         <input type="text" name="title" v-model="title" />
       </div>
+      <div v-for="(ing, index) in ingredients" :key="index">
+        <label for="ingredient">Ingredient</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]" />
+      </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add Ingredient</label>
-        <input type="text" name="add-ingredient" />
+        <input
+          type="text"
+          name="add-ingredient"
+          @keydown.tab.prevent="addIn"
+          v-model="anotherIng"
+        />
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -18,16 +31,53 @@
 </template>
 
 <script>
+import db from "@/firebase/init";
+import slugify from "slugify";
+
 export default {
   data() {
     return {
-      title: "",
+      title: null,
+      anotherIng: null,
+      ingredients: [],
+      feedback: null,
+      slug: null,
     };
   },
 
   methods: {
     addSmoothie() {
-      console.log(this.title);
+      if (this.title) {
+        this.feedback = null;
+        this.slug = slugify(this.title, {
+          replacement: "-",
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true,
+        });
+        db.collection("smoothies")
+          .add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug,
+          })
+          .then(() => {
+            this.$router.push("/");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        this.feedback = "You must enter a smoothie title";
+      }
+    },
+    addIn() {
+      if (this.anotherIng) {
+        this.ingredients.push(this.anotherIng);
+        this.anotherIng = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value to add your ingredient";
+      }
     },
   },
 };
